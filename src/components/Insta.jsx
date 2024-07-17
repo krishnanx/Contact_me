bd264f481d6e36c8e065d2a5d1f6a19c3cbf0d6a
krishnanx <abhishekkrishnan2006@gmail.com>
2024-07-17 21:20:51 +0530

    social media pages link is componentized

---
+++ src/components/Insta.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Box } from '@chakra-ui/react'
+const Insta = () => {
+  return (
+    <Box
+        as="svg"
+        //as="svg"
+        viewBox="0 0 16 15.999"
+        height="16"
+        width="75px"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="white"
+        _hover={{fill:"red"}}
+        >
+        <path
+            as="path"
+            transform="translate(6 598)"
+            d="M6-582H-2a4,4,0,0,1-4-4v-8a4,4,0,0,1,4-4H6a4,4,0,0,1,4,4v8A4,4,0,0,1,6-582ZM2-594a4,4,0,0,0-4,4,4,4,0,0,0,4,4,4,4,0,0,0,4-4A4.005,4.005,0,0,0,2-594Zm4.5-2a1,1,0,0,0-1,1,1,1,0,0,0,1,1,1,1,0,0,0,1-1A1,1,0,0,0,6.5-596ZM2-587.5A2.5,2.5,0,0,1-.5-590,2.5,2.5,0,0,1,2-592.5,2.5,2.5,0,0,1,4.5-590,2.5,2.5,0,0,1,2-587.5Z"
+            data-name="Subtraction 4"
+            
+            id="Subtraction_4"
+        ></path>
+    </Box>
+  )
+}
+
+export default Insta
